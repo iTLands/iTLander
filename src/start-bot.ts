edc4950c5cbdc994d3c419930a42b4483d942d8d
JosueIsOffline <josuehernandez2314@gmail.com>
2025-08-31 01:10:08 -0400

    feat: create main file to init the discord bot

---
+++ src/start-bot.ts
@@ -0,0 +1,37 @@
+import { CustomClient } from "./extensions/custom-client";
+import { Config } from "./config";
+import { GatewayIntentBits } from "discord.js";
+import { Bot } from "./models/bot";
+import { Logger } from "./services";
+import { DatabaseFactory } from "./services/database/factory-db";
+
+async function start(): Promise<void> {
+  const client = new CustomClient({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMembers,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.DirectMessages,
+      GatewayIntentBits.DirectMessageReactions,
+    ],
+    enforceNonce: true,
+  });
+
+  const bot = new Bot(Config.discord.token!, client, Config.discord.guildId!);
+  await bot.start();
+
+  if (Config.database.enabled) {
+    const db = DatabaseFactory.type();
+    db.init();
+  }
+}
+
+process.on("unhandledRejection", (reason, _promise) => {
+  Logger.error("Unhandled rejection at:", reason);
+});
+
+start().catch((error) => {
+  Logger.error("Failed to start bot:", error);
+  process.exit(1);
+});
